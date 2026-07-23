59b2c610678cd95152f75970555e657346e658f3
dsaido1 <diyarsaido7@gmail.com>
2026-07-23 15:28:38 +0100

    Add shopping list project

---
+++ shoppingList.py
@@ -0,0 +1,13 @@
+groceries = []
+
+item1 = input ("Please enter your first item: ")
+item2 = input ("Please enter your second item: ")
+item3 = input ("Please enter your third item: ")
+
+groceries.append(item1)
+groceries.append(item2)
+groceries.append(item3)
+
+
+print(groceries)
+print(len(groceries))
