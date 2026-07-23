4fb50a3f4594025e919021583af00fc9753a3329
dsaido1 <diyarsaido7@gmail.com>
2026-07-23 15:08:10 +0100

    Add calculator project

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
+num1 = int(input("first number: "))
+num2 = int(input("second number: "))
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+operation = input("choose one of those *+-/ ")
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if operation == "*":
+   result = num1 * num2
+   print(result)
+elif operation == "+":
+   result = num1 + num2
+   print(result)
+elif operation == "-":
+   result = num1 - num2
+   print(result)
+elif operation == "/":
+   result =num1 / num2
+   print(result)
+else: print("invalid operation")
