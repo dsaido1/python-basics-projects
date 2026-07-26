f3499bd111b4ec9d5e76b947c906ed980871b63b
dsaido1 <diyarsaido7@gmail.com>
2026-07-26 20:10:59 +0100

    Add file counter project

---
+++ file_counter.py
@@ -0,0 +1,8 @@
+print("Welcome to file counter")
+files = ["photo.png", "notes.txt", "video.mp4", "resume.pdf"]
+
+counter = 0
+for file in files:
+    counter += 1
+print("The total number of files is: ", counter)
+
